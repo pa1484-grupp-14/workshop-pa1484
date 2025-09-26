0c01b4990aa97f1f2bc16acd4a2450f5f9685190
Urgobalt <ludvigkallqvistnygren@gmail.com>
2025-09-26 11:47:52 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = with pkgs;
+        mkShell {
+          packages = [
+            cmake
+            clang
+            clang-tools
+          ];
+          shellHook = ''
+          '';
+        };
+    });
+}
