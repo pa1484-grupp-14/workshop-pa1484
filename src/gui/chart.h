86fa48ab37bd32ec42703b862dc1a51ab6858d2c
Vickerinox <05viktorinox@gmail.com>
2025-11-06 15:01:09 +0100

    Add chart division lines and series removal methods
    Introduced setDivisionLines and removeSeries methods to the Chart class for enhanced chart customization and management. Also added addEventCallback to Widget for easier event handling. Updated header files with documentation for new methods.

--- src/gui/chart.h
+++ src/gui/chart.h
@@ -8,10 +8,31 @@ class Chart: public Widget {
     public:
     ~Chart();
     Chart(WidgetContainer& parent, uint32_t id);   
+    /// # Set the type of this chart
+    /// DEFAULT: the default chart type is a line graph
     Chart& setType(lv_chart_type_t type);
+    
+    /// # Set the number of division lines in the chart
+    Chart& setDivisionLines(uint32_t horizontal_lines, uint32_t vertical_lines);
+
+    /// # Add a series to the chart with name `name`
+    /// NOTE: by default 10 points are automatically allocated
     Chart& addSeries(std::string& name, lv_color_t color, lv_chart_axis_t axis);
+
+    /// # Remove the series with the name `name`
+    /// EXCEPTIONS: if the specified series doesn't exist in the chart, an out of bound exception will be thrown.
+    Chart& removeSeries(std::string& name);
+
+    /// # Add a new point to the chart with name `series_name`
+    /// NOTE: the series must previously have been added with the ``Chart::addSeries`` method.
+    /// NOTE: remember to call ``Chart::refresh`` after using this method!
+    /// EXCEPTIONS: if the specified series doesn't exist in the chart, an out of bound exception will be thrown.
     Chart& addPoint(std::string& series_name, int32_t value);
+
+    /// # Updates the chart to display the current state of all the included series. 
     Chart& refresh();
+
+    /// Set the cursor position in the chart
     Chart& setCursorPos(lv_chart_cursor_t *cursor, lv_point_t *pos);
 };
 
