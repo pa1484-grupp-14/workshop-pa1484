e84ca0017683306ceb6dd587dc81af56f4c7cd53
Vickerinox <05viktorinox@gmail.com>
2025-11-06 15:27:01 +0100

    Dummy commit

--- src/gui/image.cpp
+++ src/gui/image.cpp
@@ -1,4 +1,5 @@
 #include "gui.h"
+
 Image::Image(WidgetContainer& parent, u_int32_t id): Widget(parent.getTile(), lv_image_create(parent.getWidgetPtr()), id) {}
 Image& Image::setSource(lv_image_dsc_t* src) {
     lv_image_set_src(this->getWidgetPtr(), src);
@@ -7,4 +8,4 @@ Image& Image::setSource(lv_image_dsc_t* src) {
 Image& Image::setScale() {
     lv_image_set_scale();
     return *this;
-}
+} 
