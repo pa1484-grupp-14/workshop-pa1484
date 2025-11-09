ea2855424e4b3c44555883e22c2ef3b943f67b58
TheAzzieDev <asrielrosemary@gmail.com>
2025-11-09 22:51:08 +0100

    demo in javascript changed to suit current requirements, Readme is not yet updated

--- project/apiLogic/demoInJavascript/API.js
+++ project/apiLogic/demoInJavascript/API.js
@@ -13,96 +13,135 @@
 */
 
 //StationsName = stations name attribute not key, typeOfData = parameter
-getStationInfo = async(stationName, typeOfData) => {
+
+baseURLHistorical = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter";
+baseURLForecast = "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon";
+
+
+getStationsArray = async(parameter) =>
+{
     let promise = new Promise(resolve =>
     {
-        fetch(`https://opendata-download-metobs.smhi.se/api/version/latest/parameter/${typeOfData}.json`)
+        fetch(`${baseURLHistorical}/${parameter}.json`)
         .then(response => response.json())
         .then(response =>  {
-            counter = 0;
-            condition = true;
-            while(condition)
-            {
-                if(counter == response["station"].length)
-                    throw("station does not exist")
+            resolve(response["station"]);
+        });
+    });
+    value = await promise;
+    return value;
+}
 
-                if(response["station"][counter]["name"] == stationName)
+getStationFromArray = (array, stationName) => {
+    counter = 0;
+    while(counter != array.length)
+    {
+        let theName = array[counter]["name"];
+        if(theName == stationName)
+        {
+            return array[counter];
+        }
+        counter++;
+    }
+}
+
+
+//("Abisko Aut", 1, "latest-months", 2)
+getHistoricalData = async(key, parameter) => {
+    let promise = new Promise(resolve =>
+    {
+        let data = [];
+        let valueToAdd = 0;
+        let counter = 0;
+        fetch(`${baseURLHistorical}/${parameter}/station/${key}/period/latest-months/data.json`)
+        .then(response => response.json())
+        .then(response =>  {
+            let dataValues = response["value"];
+            let dateCompare = new Date(dataValues[0]["date"]);
+            let cond = dataValues[0]["date"];
+            if(cond != undefined)
+            {
+                for(i = 0; i < dataValues.length; i++)
                 {
-                    resolve({"stationName": response["station"][counter]["name"] , "key": response["station"][counter]["key"]});  
-                    condition = false;
+                    let date = new Date(dataValues[i]["date"]);
+                    if(date.getDate() == dateCompare.getDate())
+                    {
+                        counter++;
+                        valueToAdd += parseFloat(dataValues[i]["value"]); 
+                    } 
+                    else {
+                        valueToAdd = (valueToAdd / counter).toPrecision(2);
+                        let millis = dateCompare.getTime();
+                        data.push({"date": millis, "value": valueToAdd});
+                        counter = 0;
+                        valueToAdd = 0;
+                        dateCompare = new Date(dataValues[i]["date"]);
+                    }
+                }
+            }
+            else
+            {
+                for(i = 0; i < dataValues.length; i++)
+                {
+                    let date = new Date(dataValues[i]["from"]);
+                    let millis = date.getTime();
+                    valueToAdd += parseFloat(dataValues[i]["value"]); 
+                    data.push({"From": millis, "value": valueToAdd});
+                }
+            }
+            resolve(data); 
+        })
+        .catch(()=>{
+            console.error("option not available for station");
+        })
+    });
+    return await promise;
+}
+
+getForecastNext7Days = async(stationObj) =>{
+    longitude = stationObj["longitude"];
+    latitude = stationObj["latitude"];
+    let data = [];
+    let promise = new Promise(resolve =>
+    {
+        fetch(`${baseURLForecast}/${longitude}/lat/${latitude}/data.json`)
+        .then(response => response.json())
+        .then(response =>  {
+            let dateNow = new Date();
+            let nrOfDaysAdded = 0;
+            let timeSeries = response["timeSeries"];
+            let counter = 0;
+            while(nrOfDaysAdded != 7 && counter != timeSeries.length) 
+            {
+                let entryTime = new Date (timeSeries[counter]["time"]);
+                if(dateNow.getDate() != entryTime.getDate())
+                {
+                    if(entryTime.getUTCHours() == 12)
+                    {
+                        data.push(timeSeries[counter]);
+                        nrOfDaysAdded++;
+                    }
                 }
                 counter++;
             }
+            resolve(data);
         });
     });
     value = await promise;
     return value;
     
-}
+};
 
 
-//duration = period, intervall = how many entries from latest
-getData = async(stationName, typeOfData, duration, intervall) => {
-    stationKey = await getStationInfo(stationName, typeOfData);
-    stationKey = stationKey["key"];
-    let data = [];
-
-    let promise = new Promise(resolve =>
-    {
-        fetch(`https://opendata-download-metobs.smhi.se/api/version/latest/parameter/${typeOfData}/station/${stationKey}/period/${duration}/data.json`)
-        .then(response => response.json())
-        .then(response =>  {
-
-            lastIndex = response["value"].length - 1;
-            intervallFromLastIndex = response["value"].length - intervall;
-
-            if(intervall == 1)
-                resolve(response["value"][lastIndex]);
-
-            else 
-            {
-                for(i = lastIndex; i >= intervallFromLastIndex; i--)
-                {
-                    dataItem = response["value"][i];
-                    data.push(dataItem);
-                }
-                resolve(data);
-            }
-        });
-    })
-
-    return await promise;
-}
-
-getDataLatestMonths = async(stationName, typeOfData) => {
-    stationKey = await getStationInfo(stationName, typeOfData);
-    stationKey = stationKey["key"];
-    let data = [];
-    let promise = new Promise(resolve => 
-    {
-        fetch(`https://opendata-download-metobs.smhi.se/api/version/latest/parameter/${typeOfData}/station/${stationKey}/period/latest-months/data.json`)
-        .then(response => response.json())
-        .then(response =>  {
-    
-            lastIndex = response["value"].length - 1;
-            for(i = lastIndex; i >= 0; i--)
-            {
-                dataItem = response["value"][i];
-                data.push(dataItem);
-            }
-            resolve(data);
-        });
-    });
- 
-    return await data;
-}
-
+//Example how to Use functions down bellow
 window.addEventListener("load", async()=>{
-    object = await getData("Abisko Aut", 1, "latest-months", 2);
-    object2 = await getDataLatestMonths("Abisko Aut", 1);
-    object3 = await getStationInfo("Abisko Aut", 1);
+    let array = await getStationsArray(1);
+    let station = getStationFromArray(array,"Abisko Aut");
+    let dataHistorical = await getHistoricalData(station["key"], 1); 
+    let dataForecast =  await getForecastNext7Days(station);
 
-    console.log(object);
-    console.log(object2);
-    console.log(object3);
+    console.log(array);
+    console.log(station);
+    console.log(dataHistorical);
+    console.log(dataForecast);
 })
