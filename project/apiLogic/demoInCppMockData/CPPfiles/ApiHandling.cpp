24fdc598545316d4524d3d786451948cc11afb9a
B4z370rP <lukasanderssson22@hotmail.com>
2025-11-16 14:34:04 +0100

    Justering av funktion APIhandler::getForecastNext7Days

--- project/apiLogic/demoInCppMockData/CPPfiles/ApiHandling.cpp
+++ project/apiLogic/demoInCppMockData/CPPfiles/ApiHandling.cpp
@@ -86,50 +86,45 @@ vector<HistoricalObject> APIhandler::getHistoricalData(const string& key, int pa
 
 std::vector<ForecastObject> APIhandler::getForecastNext7Days(const StationObject&)
 {
+    std::vector<ForecastObject> result;
+
     std::string fileToRead = "forecastJson.json";
     std::string jsonStr = loadFileToString(fileToRead);
-    
+
     DynamicJsonDocument doc(256 * 1024);
-    if (deserializeJson(doc, jsonStr)) return {};
+    if (deserializeJson(doc, jsonStr)) return result;
 
     JsonArray series = doc["timeSeries"].as<JsonArray>();
-    if (series.isNull()) return {};
-
-    std::vector<ForecastObject> result;
-    result.reserve(7);
-
-    for (JsonObject entry : series)
-    {
+    if (series.isNull()) return result;
+    
+    for (JsonObject entry : series) {
         const char* timeStr = entry["time"];
         if (!timeStr || !strstr(timeStr, "12:00:00Z")) continue;
-
         JsonObject data = entry["data"];
         ForecastObject fo{};
-
         fo.air_pressure_at_mean_sea_level = data["air_pressure_at_mean_sea_level"] | 0;
-        fo.air_temperature               = data["air_temperature"] | 0.0f;
-        fo.cloud_area_fraction           = data["cloud_area_fraction"] | 0;
-        fo.cloud_base_altitude           = data["cloud_base_altitude"] | 0;
-        fo.cloud_top_altitude            = data["cloud_top_altitude"] | 0;
+        fo.air_temperature = data["air_temperature"] | 0.0f;
+        fo.cloud_area_fraction = data["cloud_area_fraction"] | 0;
+        fo.cloud_base_altitude = data["cloud_base_altitude"] | 0;
+        fo.cloud_top_altitude = data["cloud_top_altitude"] | 0;
         fo.high_type_cloud_area_fraction = data["high_type_cloud_area_fraction"] | 0;
-        fo.low_type_cloud_area_fraction  = data["low_type_cloud_area_fraction"] | 0;
+        fo.low_type_cloud_area_fraction = data["low_type_cloud_area_fraction"] | 0;
         fo.medium_type_cloud_area_fraction = data["medium_type_cloud_area_fraction"] | 0;
-        fo.precipitation_amount_max      = data["precipitation_amount_max"] | 0.0f;
-        fo.precipitation_amount_mean     = data["precipitation_amount_mean"] | 0.0f;
-        fo.precipitation_amount_median   = data["precipitation_amount_median"] | 0.0f;
-        fo.precipitation_amount_min      = data["precipitation_amount_min"] | 0.0f;
-        fo.precipitation_frozen_part     = data["precipitation_frozen_part"] | 0;
+        fo.precipitation_amount_max = data["precipitation_amount_max"] | 0.0f;
+        fo.precipitation_amount_mean = data["precipitation_amount_mean"] | 0.0f;
+        fo.precipitation_amount_median = data["precipitation_amount_median"] | 0.0f;
+        fo.precipitation_amount_min = data["precipitation_amount_min"] | 0.0f;
+        fo.precipitation_frozen_part = data["precipitation_frozen_part"] | 0;
         fo.predominant_precipitation_type_at_surface = data["predominant_precipitation_type_at_surface"] | 0;
         fo.probability_of_frozen_precipitation = data["probability_of_frozen_precipitation"] | 0;
-        fo.probability_of_precipitation   = data["probability_of_precipitation"] | 0;
-        fo.relative_humidity              = data["relative_humidity"] | 0;
-        fo.symbol_code                    = data["symbol_code"] | 0;
-        fo.thunderstorm_probability       = data["thunderstorm_probability"] | 0;
-        fo.visibility_in_air              = data["visibility_in_air"] | 0;
-        fo.wind_from_direction            = data["wind_from_direction"] | 0;
-        fo.wind_speed                     = data["wind_speed"] | 0.0f;
-        fo.wind_speed_of_gust             = data["wind_speed_of_gust"] | 0.0f;
-
+        fo.probability_of_precipitation = data["probability_of_precipitation"] | 0;
+        fo.relative_humidity = data["relative_humidity"] | 0;
+        fo.symbol_code = data["symbol_code"] | 0;
+        fo.thunderstorm_probability = data["thunderstorm_probability"] | 0;
+        fo.visibility_in_air = data["visibility_in_air"] | 0;
+        fo.wind_from_direction = data["wind_from_direction"] | 0;
+        fo.wind_speed = data["wind_speed"] | 0.0f;
+        fo.wind_speed_of_gust = data["wind_speed_of_gust"] | 0.0f;
         result.emplace_back(fo);
         if (result.size() >= 7) break;
     }
