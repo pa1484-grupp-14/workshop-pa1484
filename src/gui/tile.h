bdbbc9838961c447a6fd46d089851d06056e3b93
Vickerinox <05viktorinox@gmail.com>
2025-11-16 16:14:22 +0100

    updated wrappers and containers

--- src/gui/tile.h
+++ src/gui/tile.h
@@ -21,6 +21,7 @@ class Tile: public WidgetContainer {
     std::unordered_map<std::string, Image> images;
     std::unordered_map<std::string, Dropdown> dropdowns;
     std::unordered_map<std::string, Chart> charts;
+    std::unordered_map<std::string, Container> containers;
 
     uint32_t counter;
 
@@ -31,16 +32,20 @@ class Tile: public WidgetContainer {
     Chart& createChart(WidgetContainer& parent, std::string name);
     Image& createImage(WidgetContainer& parent, std::string name);
     Dropdown& createDropdown(WidgetContainer& parent, std::string name);
+    Container& createContainer(WidgetContainer& parent, std::string name);
     Tile& getTile() override;
     WidgetContainer& getParent() override;
     Label& addLabel(std::string name = nextIdString()) override;
     Chart& addChart(std::string name = nextIdString()) override;
     Image& addImage(std::string name = nextIdString()) override;
+    Container& addContainer(std::string name = nextIdString()) override;
     Dropdown& addDropdown(std::string name = nextIdString()) override;
     Label& getLabel(std::string name) {
         return labels.at(name);
     }
+
     ~Tile();
     friend class GUI;
 };
+
 #endif
